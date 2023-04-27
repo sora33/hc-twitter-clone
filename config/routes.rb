85c99d71425cc92411f840ee2617d3855443bd93
hiranuma <shuuuya0616@gmail.com>
2023-04-27 22:04:57 +0900

    本タスクのスコープ外のコードをコメント

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # devise_for :users, controllers: { sessions: 'users/sessions' }
   root to: 'home#index'
   devise_for :users, controllers: { registrations: 'users/registrations', confirmations: 'users/confirmations' }
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+  # mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
