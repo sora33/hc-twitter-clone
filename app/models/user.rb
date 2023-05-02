308909ab9be9657c22c3e1dcd68557b92d0fff14
hiranuma <shuuuya0616@gmail.com>
2023-05-03 08:38:25 +0900

    既存登録がある場合の処理を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,43 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable,
-         :omniauthable, omniauth_providers: [:github]
+          :recoverable, :rememberable, :validatable, :confirmable,
+          :omniauthable, omniauth_providers: [:github]
 
   validates :tel, presence: true, unless: :from_omniauth?
   validates :birthday, presence: true, unless: :from_omniauth?
 
-  # authはGitHubから送られてくるハッシュ。名前やメアドなどが入っている。
-  # DBにデータなければ新規作成、見つかればそのデータを返す
   def self.find_for_github_oauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
-      user.email = auth.info.email # user.name = auth.info.name
-      user.password = Devise.friendly_token[0, 20] # 任意の20文字の文字列を作成する
-      user.skip_confirmation! # 仮登録メールを介さずに即時登録
+    # 既存のGitHubアカウントを検索
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+    return user if user
+
+    # 既存のメールアドレスを持つユーザーを検索
+    user = User.where(email: auth.info.email).first_or_initialize
+
+    if user.new_record? # 新しいユーザーの場合
+      user.assign_attributes(
+        email: auth.info.email,
+        password: Devise.friendly_token[0, 20],
+        provider: auth.provider,
+        uid: auth.uid
+      )
+      user.skip_confirmation! # 確認メールをスキップ
+    else
+      # 既存のユーザーにGitHubの情報を追加
+      user.update_columns(provider: auth.provider, uid: auth.uid)
     end
+
+    user.save!
+    user
   end
+
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
+  
+  
+
 
   def from_omniauth?
     provider.present? && uid.present?
