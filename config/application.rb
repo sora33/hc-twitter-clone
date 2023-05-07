7fe3ee644887d98ff9f9399edd94d094f7ff2cd6
hiranuma <shuuuya0616@gmail.com>
2023-05-07 17:59:49 +0900

    設定を変更

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,9 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.i18n.default_locale = :en
+    config.i18n.default_locale = :jp
+    config.log_level = :debug
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
