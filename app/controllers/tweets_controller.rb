5ba144aadc7c46d61d4d2fff90a12ee2cea8355b
hiranuma <shuuuya0616@gmail.com>
2023-05-19 02:25:38 +0900

    ツイート作成機能を実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,13 +2,29 @@
 
 class TweetsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_tweets, only: %i[index create]
 
-  def index
-    @tweets = current_user.all_tweets.page(params[:page])
+  def index; end
+
+  def create
+    @tweet = current_user.tweets.build(tweet_params)
+    if @tweet.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   def following
     @tweets = current_user.following_tweets.page(params[:page])
     render :index
+  end
+
+  def tweet_params
+    params.require(:tweet).permit(:content, :image)
+  end
+
+  def set_tweets
+    @tweets = current_user.all_tweets.page(params[:page])
   end
 end
