8598e14abe25f0815e523266b214f52e47df95ed
hiranuma <shuuuya0616@gmail.com>
2023-07-04 16:07:17 +0900

    モデルのテストを記述

---
+++ spec/models/tweets_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe Tweet, type: :model do
+  let(:user) { FactoryBot.create(:user) }
+
+  it "has a valid factory with image" do
+    tweet = FactoryBot.build(:tweet, :with_image, user: user)
+    expect(tweet).to be_valid
+  end
+
+  it "has a valid factory without image" do
+    tweet = FactoryBot.build(:tweet, user: user)
+    expect(tweet).to be_valid
+  end
+
+  it "is invalid without content" do
+    tweet = FactoryBot.build(:tweet, user: user, content: nil)
+    tweet.valid?
+    expect(tweet.errors[:content]).to include("を入力してください")
+  end
+
+  it "User is invalid with a duplicate email address" do
+    tweet = FactoryBot.build(:tweet, user: user, content: "a" * 141)
+    tweet.valid?
+    expect(tweet.errors[:content]).to include("は140文字以下にしてください")
+  end
+
+  it "can have many likes" do
+    tweet = FactoryBot.create(:tweet, user: user)
+    FactoryBot.create(:like, user: user, tweet: tweet)
+    FactoryBot.create(:like, user: FactoryBot.create(:user), tweet: tweet)
+    expect(tweet.likes.count).to eq(2)
+  end
+
+  it "can have many replies" do
+    tweet = FactoryBot.create(:tweet, user: user)
+    FactoryBot.create(:tweet, user: user, parent: tweet)
+    FactoryBot.create(:tweet, user: FactoryBot.create(:user), parent: tweet)
+    expect(tweet.replies.count).to eq(2)
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,81 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryBot.build(:user)).to be_valid
+  end
+
+  it "is valid with name, birthday, email, tel, website, and password" do
+    user = User.new(
+      name: "Aaron",
+      email:      "tester@example.com",
+      tel: "1234567890",
+      birthday:   Date.today,
+      website: "http://www.example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+    expect(user).to be_valid
+  end
+  
+  it "is invalid without a password" do
+    user = FactoryBot.build(:user, password: nil)
+    user.valid?
+    expect(user.errors[:password]).to include("を入力してください")
+  end
+
+  it "is invalid without an email address" do
+    user = FactoryBot.build(:user, email: nil)
+    user.valid?
+    expect(user.errors[:email]).to include("を入力してください")
+  end
+
+  it "is invalid without an birthday address" do
+    user = FactoryBot.build(:user, birthday: nil)
+    user.valid?
+    expect(user.errors[:birthday]).to include("を入力してください")
+  end
+
+  it "is invalid without an tel address" do
+    user = FactoryBot.build(:user, tel: nil)
+    user.valid?
+    expect(user.errors[:tel]).to include("を入力してください")
+  end
+
+  it "is invalid with a duplicate email address" do
+    FactoryBot.create(:user, email: "aaron@example.com")
+    user = FactoryBot.build(:user, email: "aaron@example.com")
+    allow_any_instance_of(User).to receive(:send_confirmation_instructions).and_return(true)
+    user.valid?
+    expect(user.errors[:email]).to include("はすでに使用されています")
+  end
+
+  describe "#follow and #unfollow" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:other_user) { FactoryBot.create(:user) }
+
+    it "can follow and unfollow a user" do
+      user.follow(other_user)
+      expect(user.following?(other_user)).to be_truthy
+
+      user.unfollow(other_user)
+      expect(user.following?(other_user)).to be_falsey
+    end
+  end
+
+  describe "#create_notification" do
+    let(:user) { FactoryBot.create(:user) }
+    let(:other_user) { FactoryBot.create(:user) }
+    let(:tweet) { FactoryBot.create(:tweet, user: other_user) }
+  
+    it "creates a new notification" do
+      expect { user.create_notification(tweet, 'like') }.to change { Notification.count }.by(1)
+      
+      created_notification = Notification.last
+      expect(created_notification.action).to eq 'like'
+      expect(created_notification.action_user).to eq user
+      expect(created_notification.tweet).to eq tweet
+    end
+  end
+  
+
 end
