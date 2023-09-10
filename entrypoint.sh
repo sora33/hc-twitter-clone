c1261eff068e0c52f79e834a16b401292b95986d
hiranuma <shuuuya0616@gmail.com>
2023-09-10 15:50:58 +0900

    db:cretateを追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,30 @@
+# #!/bin/bash
+# set -e
+
+# # Remove a potentially pre-existing server.pid for Rails.
+# rm -f /myapp/tmp/pids/server.pid
+
+# # Then exec the container's main process (what's set as CMD in the Dockerfile).
+# exec "$@"
+
+
 #!/bin/bash
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
+
+# production環境の場合のみ
+if [ "$RAILS_ENV" = "production" ]; then
+  # --------------------------------------
+  # 本番環境（AWS ECS）への初回デプロイ時に利用
+  # 初回デプロイ後にコメントアウトして下さい
+  bundle exec rails db:create
+  # --------------------------------------
+  # 2回目以降のデプロイ時に利用
+  # 初回デプロイ後にコメントアウトを外して下さい
+  # bundle exec rails db:migrate
+fi
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
