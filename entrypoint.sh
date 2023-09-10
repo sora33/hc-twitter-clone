b19b833f6ba63b19edaa175c81c00f680c58ef19
hiranuma <shuuuya0616@gmail.com>
2023-09-10 20:50:15 +0900

    yarn の処理を追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,9 @@ rm -f /myapp/tmp/pids/server.pid
 
 # production環境の場合のみ
 if [ "$RAILS_ENV" = "production" ]; then
-  bundle exec rails assets:clobber
-  bundle exec rails assets:precompile
+  yarn install --check-files
+  yarn build
+  yarn build:css
   # --------------------------------------
   # 本番環境（AWS ECS）への初回デプロイ時に利用
   # 初回デプロイ後にコメントアウトして下さい
