d7ee669e136443cb24515a0c18b995558cb42fee
hiranuma <shuuuya0616@gmail.com>
2023-09-10 20:30:40 +0900

    アセットコンパイルを追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,14 @@ rm -f /myapp/tmp/pids/server.pid
 
 # production環境の場合のみ
 if [ "$RAILS_ENV" = "production" ]; then
+  bundle exec rails assets:clobber
+  bundle exec rails assets:precompile
   # --------------------------------------
   # 本番環境（AWS ECS）への初回デプロイ時に利用
   # 初回デプロイ後にコメントアウトして下さい
   # bundle exec rails db:create
   # --------------------------------------
   # 2回目以降のデプロイ時に利用
-  # 初回デプロイ後にコメントアウトを外して下さい
   bundle exec rails db:migrate
 fi
 
